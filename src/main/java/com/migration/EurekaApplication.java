002a169b8eb13649131f54d8cb3153cc08cd62ad
TemplarJQ <jqliu_bupt@163.com>
2021-04-12 16:12:17 +0800

    base eureka client code

---
+++ src/main/java/com/migration/EurekaApplication.java
@@ -0,0 +1,21 @@
+package com.migration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Hello world!
+ *
+ */
+
+@EnableEurekaClient
+@SpringBootApplication
+public class EurekaApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(EurekaApplication.class, args);
+        System.out.println( "Hello World!" );
+    }
+}
