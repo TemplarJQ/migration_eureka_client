5fc85a849e71e7be8cc0c935736b17ec801bb577
TemplarJQ <jqliu_bupt@163.com>
2021-04-12 20:43:23 +0800

    add lombok

---
+++ src/main/java/com/dataobject/UserDO.java
@@ -0,0 +1,18 @@
+package com.dataobject;
+
+
+public class UserDO {
+
+    private Integer userId;
+
+    private String userName;
+
+    private String registerMode;
+
+    private Integer user_level;
+
+    private String thirdPartyId;
+
+    private String encrptPassword;
+
+}
